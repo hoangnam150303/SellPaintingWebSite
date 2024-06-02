1c20787e9a57e00d9f1a87240da956bceabcb079
Vu Hoang Nam <hoangnam150303@gmail.com>
2024-06-02 17:13:20 +0700

    Add project files.

---
+++ SellPainting/Models/TypesOfPaiting.cs
@@ -0,0 +1,9 @@
+﻿namespace SellPainting.Models
+{
+    public class TypesOfPaiting
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
